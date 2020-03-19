a5404fe08a0236c9627f15033d2e26a415c7a9cb
Justin Llewellyn <jllew24@gmail.com>
2020-03-19 14:52:36 -0600

    add and edit articles updated

--- models/article.js
+++ models/article.js
@@ -1,37 +1,69 @@
 const mongoose = require('mongoose');
 
-const categorySchema = mongoose.Schema({
+const articleSchema = mongoose.Schema({
   title: {
     type: String
   },
-  description: {
+  subtitle: {
     type: String
-  }
+  },
+  category: {
+    type: String
+  },
+   body: {
+    type: String
+  },
+  author: {
+    type: String
+  },
+  created_at: {
+    type: Date,
+    default: Date.now
+  },
+  comments: [
+    {
+      comment_subject: {
+        type: String
+      },
+      comment_body: {
+        type: String
+      },
+      comment_author: {
+        type: String
+      },
+      comment_email: {
+        type: String
+      },
+      comment_date: {
+        type: String
+      }
+    }
+  ]
 });
 
-const Category = module.exports = mongoose.model('Category', categorySchema);
+const Article = module.exports = mongoose.model('Article', articleSchema);
 
-// Get categories
-module.exports.getCategories = (callback, limit) => {
-  Category.find(callback).limit(limit).sort([['title', 'ascending']])
+// Get article
+module.exports.getArticles = (callback, limit) => {
+  Article.find(callback).limit(limit).sort([['title', 'ascending']])
 }
 
-//Add category
-module.exports.addCategory = (category, callback) => {
-  Category.create(category, callback);
+//Add article
+module.exports.addArticle = (article, callback) => {
+  Article.create(article, callback);
 }
 
-// Get single category
-module.exports.getCategoryById = (id, callback) => {
-  Category.findById(id, callback);
+// Get single article
+module.exports.getArticleById = (id, callback) => {
+  Article.findById(id, callback);
 }
 
-// Update category
-module.exports.editCategory = (query, edit, callback) => {
-  Category.findOneAndUpdate(query, edit, callback);
+// Update article
+module.exports.editArticle = (query, edit, callback) => {
+  Article.findOneAndUpdate(query, edit, callback);
 }
 
-// Delete category
-module.exports.deleteCategory = (query, callback) => {
-  Category.remove(query, callback);
+// Delete article
+module.exports.deleteArticle = (query, callback) => {
+  Article.remove(query, callback);
 }
--- routes/articles.js
+++ routes/articles.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const Article = require('../models/article.js')
+
 router.get('/', (req, res, next) => {
   res.render('articles', {title: 'Articles'});
 });
@@ -13,4 +15,20 @@ router.get('/category/:category_id', (req, res, next) => {
   res.render('articles', {title: 'Category Articles'});
 });
 
+// Add article
+router.post('/add', (req, res, next) => {
+  const article = new Article();
+  article.title = req.body.title;
+  article.subtitle = req.body.subtitle;
+  article.category = req.body.category;
+  article.body = req.body.body;
+  article.author = req.body.author;
+
+  Article.addArticle(article, (err, article) => {
+    if (err) {
+      res.send(err);
+    }
+    res.redirect('/manage/articles');
+  })
+});
 module.exports = router;
