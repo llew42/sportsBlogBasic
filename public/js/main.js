46da5162a913f54116477a009511664d05c3cc74
Justin Llewellyn <jllew24@gmail.com>
2020-03-19 23:33:05 -0600

    edit and add article done

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,5 @@
 $(document).ready(() => {
   $('.category-delete').on('click', (e) => {
-    console.log("category has been deleted, fucking supposedly");
     $target = $(e.target);
     const id = $target.attr('data-cat-id');
     $.ajax({
@@ -8,6 +7,21 @@ $(document).ready(() => {
       url: `/categories/delete/${id}`,
       success: (response) => {
         alert('Category Removed');
+        window.location.href='/'
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    });
+  });
+  $('.article-delete').on('click', (e) => {
+    $target = $(e.target);
+    const id = $target.attr('data-article-id');
+    $.ajax({
+      type: 'DELETE',
+      url: `/article/delete/${id}`,
+      success: (response) => {
+        alert('Article Removed');
         window.location.href='/'
       },
       error: (error) => {
--- routes/articles.js
+++ routes/articles.js
@@ -31,4 +31,37 @@ router.post('/add', (req, res, next) => {
     res.redirect('/manage/articles');
   })
 });
+
+// Edit article
+router.post('/edit/:id', (req, res, next) => {
+  const article = new Article();
+  const query = {_id: req.params.id};
+  const edit = {
+      title: req.body.title,
+      subtitle: req.body.subtitle,
+      category: req.body.category,
+      body: req.body.body,
+      author: req.body.author
+  }
+
+  Article.editArticle(query, edit, (err, article) => {
+    if (err) {
+      res.send(err);
+    }
+    res.redirect('/manage/articles');
+  });
+});
+
+// Delete article
+router.delete('/delete/:id', (req, res, next) => {
+  const query = {_id: req.params.id};
+
+  Article.deleteArticle(query, (err, article) => {
+    if (err) {
+      res.send(err);
+    }
+    res.status(200);
+  });
+});
+
 module.exports = router;
